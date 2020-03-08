98e5bff2897e31ad2c2c16307936f18e687f99ce
Jacob Franklin <jacobfranklin217@gmail.com>
2020-03-07 21:37:55 -0600

    first commit

---
+++ app/src/main/java/com/google/apps/sanctified/GardenActivity.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.apps.sanctified
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.apps.sanctified.databinding.ActivityGardenBinding
+
+class GardenActivity : AppCompatActivity() {
+    private var content: FrameLayout? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.prayer_navigation_menu_item -> {
+                val fragment = FragmentPrayer.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.praise_navigation_menu_item -> {
+                val fragment = FragmentPraise.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.read_navigation_menu_item -> {
+                val fragment = FragmentRead.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.plans_navigation_menu_item -> {
+                val fragment = FragmentPlans.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.memory_navigation_menu_item -> {
+                val fragment = FragmentMemory.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        return@OnNavigationItemSelectedListener false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivityGardenBinding>(this, com.google.apps.sanctified.R.layout.activity_sanctified)
+
+        content = findViewById(R.id.content)
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val fragment = FragmentRead.Companion.newInstance()
+        addFragment(fragment)
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+                .commit()
+    }
+}
